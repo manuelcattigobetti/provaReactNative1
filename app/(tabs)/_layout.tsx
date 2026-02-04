b06e0decd2d45e97a8c8face9dcea53a88135b0c
manuelcattigobetti <s.manuel.catti@gobettire.istruzioneer.it>
2026-02-04 10:39:07 +0100

    Secondo commit, sempre di prova nulla di concreto

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,11 +5,19 @@ import {Ionicons} from '@expo/vector-icons'
 
 const TabsLayout = () => {
   return (
-    <Tabs screenOptions={{tabBarActiveTintColor: "red", tabBarActiveBackgroundColor: "green"}}>
-      <Tabs.Screen name='index' options={{title: "Todos", tabBarIcon: ({color, size}) => ( <Ionicons name='flash-outline' size={size}/>)}} />
-      <Tabs.Screen name='settings' options={{title: "Settings", tabBarIcon: ({color, size}) => ( <Ionicons name='settings' size={size}/>)}} />
+    <Tabs screenOptions={{tabBarActiveTintColor: "red", tabBarInactiveBackgroundColor: "green", 
+                          tabBarStyle: {backgroundColor: "lightblue", 
+                                        borderTopColor: "yellow",
+                                        height: 90,
+                                        paddingBottom: 10,
+                                        paddingTop: 10
+                                      }
+                        }}>
+      <Tabs.Screen name='index' options={{title: "", tabBarIcon: ({color, size}) => ( <Ionicons name='flash-outline' size={size}/>)}} />
+      <Tabs.Screen name='settings' options={{title: "", tabBarIcon: ({color, size}) => ( <Ionicons name='settings' size={size}/>)}} />
     </Tabs>
 )
 }
+
 
 export default TabsLayout
